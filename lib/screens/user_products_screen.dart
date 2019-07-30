050a34264dd35c27cd110720b78b84b0c76c8f78
Adedeji Rotibi <dayjeerow7@gmail.com>
2019-07-30 02:49:19 +0100

    Add form base

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -5,6 +5,8 @@ import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
 
+import './edit_product_screen.dart';
+
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
   @override
@@ -17,7 +19,7 @@ class UserProductsScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-              //..
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
             },
           ),
         ],
